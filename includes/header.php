e99fca1c94dcef08c7bad728b5e783f4ba57c981
olmcp016 <olmcp016@gmail.com>
2026-09-04 09:26:46 +0800

    Update header to use parish logo as favicon

--- includes/header.php
+++ includes/header.php
@@ -15,7 +15,12 @@ $__title = isset($pageTitle) ? $pageTitle . ' | ' . APP_NAME : APP_NAME;
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600&family=Montserrat:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="<?= url('public/css/style.css') ?>">
+  <?php if (hasParishLogo()): ?>
+  <link rel="icon" type="image/png" href="<?= url('public/img/logo.png') ?>">
+  <link rel="apple-touch-icon" href="<?= url('public/img/logo.png') ?>">
+  <?php else: ?>
   <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⛪</text></svg>">
+  <?php endif; ?>
 </head>
 <body>
 <?php $__flash = getFlash(); ?>
